d580d1e56fced982c6cfce0e65844da6c471393d
michaelmillar <4921418+michaelmillar@users.noreply.github.com>
2025-08-02 17:01:54 +0100

    enums, option for voted, u64

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,7 @@
+mod consensus_engine;
+
 fn main() {
     println!("Hello, world!");
 }
 
-struct LogPair {
-    term: String,
-    command: String,
-}
 
-struct ConsensusState {
-    current_term: i64,
-    voted_for: String,
-    last_applied: i64,
-    log: Vec<LogPair>,
-    commit_index: i64
-}
